c390f26d103d145d2c9e959eb32aed325fd5e1b5
sitivi25 <tsylyvonchyk@gmail.com>
2023-06-14 12:17:54 -0400

    Initial commit

---
+++ src/test/java/NewTestCases/NewTestCase.java
@@ -0,0 +1,18 @@
+package NewTestCases;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
+
+public class NewTestCase {
+    @Test
+    public void startDriver(){
+
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\ZenBook\\IdeaProjects\\NewTestSelenium2\\src\\test\\resources\\chromedriver.exe");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+
+        ChromeDriver driver = new ChromeDriver(options);
+        driver.get("https://mvnrepository.com/artifact/org.testng/testng");
+    }
+}

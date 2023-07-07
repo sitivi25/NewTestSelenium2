47750c0b4a520a49d6f63f0304c6625d45d0cdd0
sitivi25 <tsylyvonchyk@gmail.com>
2023-07-07 12:30:56 -0400

    dropdownlesson

---
+++ src/test/java/DropdownTest.java
@@ -0,0 +1,23 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+public class DropdownTest extends BaseTest {
+    public String websiteURL = "https://test.my-fork.com/register";
+
+    @Test
+    public void DropdownListHW() {
+        driver.get("https://test.my-fork.com/register");
+        WebElement searchDropdownElement;
+        searchDropdownElement = driver.findElement(By.xpath("//select[@id='job-title']"));
+        Select searchDropdown = new Select(searchDropdownElement);
+        List<WebElement> options;
+        options = searchDropdown.getOptions();
+        for (int i = 0; i < options.size(); i++) {
+            System.out.println(options.get(i).getText());
+        }
+    }
+}

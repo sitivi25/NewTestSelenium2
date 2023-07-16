070f94bdd0144ace5b98c1e216b994782401e525
sitivi25 <tsylyvonchyk@gmail.com>
2023-07-15 22:56:57 -0400

    add wait30 method

--- src/main/java/pageobjects/BaseMain.java
+++ src/main/java/pageobjects/BaseMain.java
@@ -5,7 +5,10 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WindowType;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,12 +23,15 @@ public class BaseMain{
         List<String> tabHandler = new ArrayList<>(driver.getWindowHandles());
         tabHandler = new ArrayList<>(driver.getWindowHandles());
         driver.switchTo().window(tabHandler.get(tab));
-
     }
     public void scroll(int pixels){
         JavascriptExecutor js = (JavascriptExecutor) driver;
         js.executeScript("window.scrollBy(0,"+pixels+")", "");
         //js.executeScript("window.scrollBy(0,-2500)", "");
     }
+    public void wait30(int sec){
+        WebDriverWait waitForDropdownToLoad = new WebDriverWait(driver, Duration.ofSeconds(30));
+        waitForDropdownToLoad.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@id='loginButton']/button")));
 
+    }
 }
